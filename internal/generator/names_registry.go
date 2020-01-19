f4cd56748fcef4b2aa0896bb7e748beacedee30e
v0xpopuli <vadim.rozhkalns@gmail.com>
2020-01-19 11:13:33 +0200

    Make some changes according to `go report card` linter, git preferences update as well

--- internal/generator/names_registry.go
+++ internal/generator/names_registry.go
@@ -15,18 +15,22 @@ type NamesRegistry struct {
 	RepositoryPackageName string
 }
 
+// GetInterfaceNames returns all names belongs to interface block
 func (nr *NamesRegistry) GetInterfaceNames() (string, string, string) {
 	return nr.InterfaceName, nr.EntityName, nr.FullPackageName
 }
 
+// GetStructNames returns all names belongs to struct block
 func (nr *NamesRegistry) GetStructNames() string {
 	return nr.StructName
 }
 
+// GetConstructorNames returns all names belongs to constructor block
 func (nr *NamesRegistry) GetConstructorNames() (string, string, string) {
 	return nr.ConstructorName, nr.InterfaceName, nr.StructName
 }
 
+// GetMethodListNames returns all names belongs to method list
 func (nr *NamesRegistry) GetMethodListNames() (string, string) {
 	return nr.ReceiveName, nr.EntityNameWithPackage
 }
