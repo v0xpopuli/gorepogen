ed50288a5a0077d2820ebfdd839bd275c928e1dd
v0xpopuli <vadim.rozhkalns@gmail.com>
2020-01-22 00:07:55 +0200

    Separate method definition to functions

--- internal/repocomp/method_funcs_bodies.go
+++ internal/repocomp/method_funcs_bodies.go
@@ -6,12 +6,37 @@ import (
 	j "github.com/dave/jennifer/jen"
 )
 
+func findAllMethod(receiverName, entityNameWithPackage string) method {
+	return method{
+		receiverName: j.Id(receiverName),
+		methodName:   findAllMethodName,
+		args:         nil,
+		returnParams: j.List(j.Index().Id(entityNameWithPackage), j.Error()),
+		fnBody:       findAllFuncBody(entityNameWithPackage),
+	}
+}
+
 func findAllFuncBody(entityName string) *j.Statement {
 	return j.Var().Id(entitiesLit).Index().Id(entityName).
 		Line().
 		Id(fmt.Sprintf("%s := r.DB.Find(&%s).%s", errLit, entitiesLit, errorLit)).
 		Line().
 		Return(j.Id(entitiesLit), j.Id(errLit))
+}
+
+func findByIdMethod(receiverName, entityNameWithPackage string) method {
+	return method{
+		receiverName: j.Id(receiverName),
+		methodName:   findByIdMethodName,
+		args: []statementPair{
+			{
+				ArgName: j.Id(idLit),
+				ArgType: j.Uint(),
+			},
+		},
+		returnParams: j.List(j.Id(entityNameWithPackage), j.Error()),
+		fnBody:       findByIdFuncBody(entityNameWithPackage),
+	}
 }
 
 func findByIdFuncBody(entityName string) *j.Statement {
@@ -22,18 +47,73 @@ func findByIdFuncBody(entityName string) *j.Statement {
 		Return(j.Id(entityLit), j.Id(errLit))
 }
 
+func saveMethod(receiverName, entityNameWithPackage string) method {
+	return method{
+		receiverName: j.Id(receiverName),
+		methodName:   saveMethodName,
+		args: []statementPair{
+			{
+				ArgName: j.Id(entityLit),
+				ArgType: j.Id(entityNameWithPackage),
+			},
+		},
+		returnParams: j.List(j.Id(entityNameWithPackage), j.Error()),
+		fnBody:       saveFuncBody(),
+	}
+}
+
 func saveFuncBody() *j.Statement {
 	return j.Id(fmt.Sprintf("%s := r.DB.Create(&%s).%s", errLit, entityLit, errorLit)).
 		Line().
 		Return(j.Id(entityLit), j.Id(errLit))
 }
 
+func updateMethod(receiverName, entityNameWithPackage string) method {
+	return method{
+		receiverName: j.Id(receiverName),
+		methodName:   updateMethodName,
+		args: []statementPair{
+			{
+				ArgName: j.Id(entityLit),
+				ArgType: j.Id(entityNameWithPackage),
+			},
+		},
+		returnParams: j.Error(),
+		fnBody:       updateFuncBody(),
+	}
+}
+
 func updateFuncBody() *j.Statement {
 	return j.Return(j.Id(fmt.Sprintf("r.DB.UpdateColumns(&%s).%s", entityLit, errorLit)))
 }
 
+func deleteMethod(receiverName, entityNameWithPackage string) method {
+	return method{
+		receiverName: j.Id(receiverName),
+		methodName:   deleteMethodName,
+		args: []statementPair{
+			{
+				ArgName: j.Id(entityLit),
+				ArgType: j.Id(entityNameWithPackage),
+			},
+		},
+		returnParams: j.Error(),
+		fnBody:       deleteFuncBody(),
+	}
+}
+
 func deleteFuncBody() *j.Statement {
 	return j.Return(j.Id(fmt.Sprintf("r.DB.Delete(&%s).%s", entityLit, errorLit)))
+}
+
+func countMethod(receiverName, entityNameWithPackage string) method {
+	return method{
+		receiverName: j.Id(receiverName),
+		methodName:   countMethodName,
+		args:         nil,
+		returnParams: j.List(j.Uint(), j.Error()),
+		fnBody:       countFuncBody(entityNameWithPackage),
+	}
 }
 
 func countFuncBody(entityName string) *j.Statement {
